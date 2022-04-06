c0a2f5135b4010e31235f9d4c1a1dffe1d61a95a
Vishv1610 <103111082+Vishv1610@users.noreply.github.com>
2022-04-06 16:14:18 +0530

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $str1='Hello Text
-    Multiple Line
-    Text within single quoted string';
-    $str2='Using double "Quote" directly inside single quoted';
-    $str3='Using escape sequence \n in single quoted string';
-    echo "<h2>$str1<br/>$str2<br/>$str3</h2>";
-?>

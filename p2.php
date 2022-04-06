1b2f9afd354e05876361962264c013ee237a7b6e
Vishv1610 <103111082+Vishv1610@users.noreply.github.com>
2022-04-06 15:49:45 +0530

    Add files via upload

---
+++ p2.php
@@ -0,0 +1,8 @@
+<?php
+    $str1='Hello Text
+    Multiple Line
+    Text within single quoted string';
+    $str2='Using double "Quote" directly inside single quoted';
+    $str3='Using escape sequence \n in single quoted string';
+    echo "<h2>$str1<br/>$str2<br/>$str3</h2>";
+?>
